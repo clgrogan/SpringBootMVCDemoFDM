e0f4c2e8954cc195ed48ad2aaa3198543a20d4ed
Curt Grogan <curt.grogan@fdmgroup.com>
2024-01-29 09:53:01 -0500

    first commit

--- src/main/java/com/fdm/demo/SpringDemo1Application.java
+++ src/main/java/com/fdm/demo/SpringDemo1Application.java
@@ -15,10 +15,10 @@ import com.fdm.demo.contact.ContactService;
 import com.fdm.demo.contact.InfoType;
 
 @SpringBootApplication
-public class SpringDemo1Application implements CommandLineRunner{
+public class SpringDemo1Application implements CommandLineRunner {
 
 	ApplicationContext appContext;
-	
+
 	public SpringDemo1Application(ApplicationContext appContext) {
 		super();
 		this.appContext = appContext;
@@ -34,17 +34,33 @@ public class SpringDemo1Application implements CommandLineRunner{
 		List<ContactInfo> johnsContactInfo = new ArrayList<>();
 		Contact johnContact = new Contact("John Wick", johnsContactInfo);
 		johnsContactInfo.add(new ContactInfo(0L, "+1234567890", InfoType.CELL_PHONE, johnContact));
-		
+
 		Optional<Contact> createdContactJohn = contactService.createContact(johnContact);
-		
-		if(createdContactJohn.isPresent()) {
-			System.out.println("\n\nsyso createdContactJohn: \n\t"+ createdContactJohn);
+
+		if (createdContactJohn.isPresent()) {
+			System.out.println("\n\nsyso createdContactJohn: \n\t" + createdContactJohn);
 		}
-		
+
 		List<Contact> allContacts = contactService.getAll();
 		System.out.println("\nallContacts:\n\n" + allContacts);
+
+		System.out.println("\n\nsyso contactService.readContactById(johnContact.getId()):\n\t"
+				+ contactService.readContactById(johnContact.getId()));
+
+		// More beans @Scope prototype demo coming up.
+		Contact fdmContact = appContext.getBean("fdmContact", Contact.class);
+		System.out.println("fdmContact: \n\t" + fdmContact);
+
+		Contact fdmContactUsa = appContext.getBean("fdmContact", Contact.class);
+		System.out.println("fdmContactUsa: \n\t" + fdmContactUsa);
 		
-		System.out.println("\n\nsyso contactService.readContactById(johnContact.getId()):\n\t"+ contactService.readContactById(johnContact.getId()));
+		fdmContactUsa.setName("FDM USA");
+		System.out.println("\n\nExecute: fdmContactUsa.setName(\"FDM USA\");");
+		System.out.println("fdmContact: \n\t" + fdmContact);
+		System.out.println("fdmContactUsa: \n\t" + fdmContactUsa);
+		
+		Contact fdmContactByName = appContext.getBean("fdmContact", Contact.class);
+
 	}
 
 }
--- src/main/java/com/fdm/demo/config/ContactCongfig.java
+++ src/main/java/com/fdm/demo/config/ContactCongfig.java
@@ -1,11 +1,14 @@
 package com.fdm.demo.config;
 
 import java.util.Map;
+import java.util.ArrayList;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import com.fdm.demo.contact.Contact;
+import com.fdm.demo.contact.ContactInfo;
 import com.fdm.demo.contact.ContactMapRepository;
 import com.fdm.demo.contact.ContactRepository;
 import com.fdm.demo.contact.ContactService;
@@ -21,5 +24,18 @@ public class ContactCongfig {
 	ContactMapRepository contactRepo(Map<Long, Contact> contacts) {
 		return new ContactMapRepository(contacts);
 	}
+	
+	@Bean("fdmContact")
+	@Scope("prototype")
+	Contact fdmGroup() {
+		return new Contact("FDM Group", new ArrayList<ContactInfo>());
+	}
+	
+	@Bean("genericContact")
+	@Scope("prototype")
+	Contact genericContact() {
+		return new Contact("Contact Name", new ArrayList<ContactInfo>());
+	}
+	
 	
 }
